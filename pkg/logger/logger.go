b5e8f5f845a832ed4892bd0416ffcb97b3c62279
Максим Руденко <limos.00@mail.ru>
2025-11-14 19:43:49 +0300

    #10 - добавил логгер

---
+++ pkg/logger/logger.go
@@ -0,0 +1,36 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+)
+
+func New(level string, format string) *slog.Logger {
+	var logLevel slog.Level
+	switch level {
+	case "debug":
+		logLevel = slog.LevelDebug
+	case "info":
+		logLevel = slog.LevelInfo
+	case "warn":
+		logLevel = slog.LevelWarn
+	case "error":
+		logLevel = slog.LevelError
+	default:
+		logLevel = slog.LevelInfo
+	}
+
+	opts := &slog.HandlerOptions{
+		Level:     logLevel,
+		AddSource: true,
+	}
+
+	var handler slog.Handler
+	if format == "json" {
+		handler = slog.NewJSONHandler(os.Stdout, opts)
+	} else {
+		handler = slog.NewTextHandler(os.Stdout, opts)
+	}
+
+	return slog.New(handler)
+}
