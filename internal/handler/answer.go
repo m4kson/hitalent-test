1b17ae842b936242c7bd7417452b74f93ea1382b
Максим Руденко <limos.00@mail.ru>
2025-11-14 18:46:57 +0300

    #9 - добавил handler'ы

---
+++ internal/handler/answer.go
@@ -0,0 +1,84 @@
+package handler
+
+import (
+	"encoding/json"
+	"hitalent-test/internal/domain"
+	"hitalent-test/internal/service"
+	"log/slog"
+	"net/http"
+	"strconv"
+)
+
+type AnswerHandler struct {
+	service *service.AnswerService
+	logger  *slog.Logger
+}
+
+func NewAnswerHandler(service *service.AnswerService, logger *slog.Logger) *AnswerHandler {
+	return &AnswerHandler{
+		service: service,
+		logger:  logger,
+	}
+}
+
+func (h *AnswerHandler) Create(w http.ResponseWriter, r *http.Request) {
+	requestID := r.Context().Value("request_id").(string)
+
+	idStr := r.PathValue("id")
+	questionID, err := strconv.ParseUint(idStr, 10, 32)
+	if err != nil {
+		HandleError(w, h.logger, domain.ErrInvalidInput, requestID)
+		return
+	}
+
+	var req domain.CreateAnswerRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		HandleError(w, h.logger, domain.ErrInvalidInput, requestID)
+		return
+	}
+
+	answer, err := h.service.Create(uint(questionID), &req)
+	if err != nil {
+		HandleError(w, h.logger, err, requestID)
+		return
+	}
+
+	respondJSON(w, http.StatusCreated, answer)
+}
+
+func (h *AnswerHandler) GetByID(w http.ResponseWriter, r *http.Request) {
+	requestID := r.Context().Value("request_id").(string)
+
+	idStr := r.PathValue("id")
+	id, err := strconv.ParseUint(idStr, 10, 32)
+	if err != nil {
+		HandleError(w, h.logger, domain.ErrInvalidInput, requestID)
+		return
+	}
+
+	answer, err := h.service.GetByID(uint(id))
+	if err != nil {
+		HandleError(w, h.logger, err, requestID)
+		return
+	}
+
+	respondJSON(w, http.StatusOK, answer)
+}
+
+func (h *AnswerHandler) Delete(w http.ResponseWriter, r *http.Request) {
+	requestID := r.Context().Value("request_id").(string)
+
+	idStr := r.PathValue("id")
+	id, err := strconv.ParseUint(idStr, 10, 32)
+	if err != nil {
+		HandleError(w, h.logger, domain.ErrInvalidInput, requestID)
+		return
+	}
+
+	if err := h.service.Delete(uint(id)); err != nil {
+		HandleError(w, h.logger, err, requestID)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
